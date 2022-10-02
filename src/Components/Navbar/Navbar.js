1b9ea6e4f6fcb896fbed19699683e2805e71e52c
Shashank Karan <64465483+shashankkaran@users.noreply.github.com>
2022-10-03 00:49:14 +0530

    Revert "navbar added"

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,22 +1,8 @@
 import React from 'react'
-import {Link} from "react-router-dom";
-import "./Navbar.css"
-import Logo from "../../assets/logo.svg"
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      <div className="Navbar_logo">
-        <Link to="/"><img src={Logo} className="logo"/></Link>
-      </div>
-      <div className="Navbar_links">
-        <Link to="/aboutUs" className="Navbar_link">About Us</Link>
-        <Link to="/gallery"className="Navbar_link">Gallery</Link>
-        <Link to="/events"className="Navbar_link">Events</Link>
-        <Link to="/team"className="Navbar_link">Team</Link>
-        <Link to="/contact"className="Navbar_link">Contact</Link>
-      </div>
-    </div>
+    <div>Navbar</div>
   )
 }
 
