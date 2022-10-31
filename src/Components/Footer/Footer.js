495345ea75793a40f7f47b03f091d87401b7cbc5
Vinit Kumar <vinitkumar.tisri@gmail.com>
2022-10-31 14:05:41 +0530

    added categories

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,6 +7,18 @@ function Footer() {
       <h1 style={{textAlign:'center'}}>
         Footer
       </h1>
+      <div style={{display:'flex'}}>
+        <h3>
+          subcategory1
+        </h3>
+        <h3>
+          subcategory2
+        </h3>
+        <h3>
+          subcategory3
+        </h3>
+      
+        </div>
 
 
     </div>
