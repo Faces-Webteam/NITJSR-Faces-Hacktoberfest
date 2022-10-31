6f59234f9b72bd4f922b635797a49b3685536efb
sKaiCzar <ujjwalgyan5@gmail.com>
2022-10-31 15:49:41 +0530

    Gallery added-skz

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
-function Gallery() {
+import ImageSlider from "./ImageSlider";
+const Gallery = () => {
+  const slides = [
+    { url: "http://localhost:3000/image-1.jpg", title: "beach" },
+    { url: "http://localhost:3000/image-2.jpg", title: "boat" },
+    { url: "http://localhost:3000/image-3.jpg", title: "forest" },
+    { url: "http://localhost:3000/image-4.jpg", title: "city" },
+    { url: "http://localhost:3000/image-5.jpg", title: "italy" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+  };
   return (
-    <div>Gallery</div>
-  )
-}
+    <div>
+      <h1>Hello monsterlessons</h1>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
+    </div>
+  );
+};
+
+
 
 export default Gallery
