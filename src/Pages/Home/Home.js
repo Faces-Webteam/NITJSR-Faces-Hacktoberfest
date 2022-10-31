4f3ed995756fc3d07bd0b2628f7f41bf8ed1f29d
sKaiCzar <ujjwalgyan5@gmail.com>
2022-10-31 16:18:42 +0530

    changes in home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Event } from './Event/Event';
+import { People } from './People/People';
+import { Workshop } from './Workshop/Workshop';
+import {Gallery} from './Gallery/Gallery';
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      <Event></Event>
+      <Gallery></Gallery>
+      <People></People>
+      <Workshop></Workshop>
+    </>
   )
 }
 
